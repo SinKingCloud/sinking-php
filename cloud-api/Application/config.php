a50cffd09bcfa8c8cc131a99a66cd30d1906ddac
SinKingCloud <56614045+SinKingCloud@users.noreply.github.com>
2023-07-04 10:34:05 +0800

    init

--- cloud-api/Application/config.php
+++
@@ -1,8 +0,0 @@
-<?php return array (
-  'database_host' => 'localhost',
-  'database_port' => '3306',
-  'database_user' => 'root',
-  'database_pwd' => 'root',
-  'database_name' => 'quick_php',
-  'database_prefix' => 'cloud_',
-);
